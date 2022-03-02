fb18309472ead61ff9fb43b9809ba75d5c937a2e
Aiden Petersen <AidenPetersen@protonmail.com>
2022-03-02 12:10:10 -0600

    Initialized project and got a basic genserver working

---
+++ lib/message_server.ex
@@ -0,0 +1,38 @@
+defmodule MessageServer do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(%{messages: ms, users: us}) do
+    {:ok, %{messages: ms, users: us}}
+  end
+
+  @impl true
+  def handle_cast({:send, m}, %{messages: ms, users: us}) do
+    # Sends the message to each client
+    Enum.each(us, fn u -> send(u, {:message, m}) end)
+
+    # Adds new message to state.
+    {:noreply, %{messages: [m | ms], users: us}}
+  end
+
+  @impl true
+  def handle_call(:join, {pid, _info}, %{messages: ms, users: us}) do
+    # Adds user to state
+    {:reply, ms, %{messages: ms, users: [pid | us]}}
+  end
+
+  @impl true
+  def handle_call(:leave, from, %{messages: ms, users: us}) do
+
+    # Removes user from state
+    new_users = Enum.filter(us, fn u -> u.pid != from.pid end)
+    {:reply, ms, %{messages: ms, users: new_users}}
+  end
+
+end
---
+++ test/mercury_test.exs
@@ -0,0 +1,26 @@
+defmodule MercuryTest do
+  use ExUnit.Case
+  doctest Mercury
+
+  test "tests genserver" do
+    {:ok, pid} = GenServer.start_link(MessageServer, %{messages: [], users: []})
+    # User 1
+    Task.async(fn ->
+      GenServer.call(pid, :join)
+      :timer.sleep(100)
+      GenServer.cast(pid, {:send, "Hello world!"})
+    end)
+    message_task = Task.async(fn ->
+      GenServer.call(pid, :join)
+      receive do
+	      {:message, m} -> m
+         end
+    end)
+    message = Task.await(message_task)
+    assert message == "Hello world!";
+  end
+
+  test "greets the world" do
+    assert Mercury.hello() == :world
+  end
+end
